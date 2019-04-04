d0823cfb2c96710d65c4af584baef4e76217b119
zachary-vincent <45057145+ZachieV@users.noreply.github.com>
2019-04-03 20:58:36 -0700

    Getting there

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,23 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(attrs) {
+    this.name = attrs.name;
+    this.age = attrs.age;
+    this.location = attrs.location;
+    this.gender = attrs.gender;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`
+  }
+}
+
+class Instructor extends Person {
+  constructor(iAttrs) {
+    this.specialty = iAttrs.specialty;
+    this.favLanguage = iAttrs.favLanguage;
+    this.catchPhrase = iAttrs.catchPhrase;
+  }
+  demo() {
+
+  }
+}
